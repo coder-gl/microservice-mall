c361329334f03c9fb05c7542473ec123e514afe1
fujingfeng-fjf <1291820914@qq.com>
2020-12-15 23:17:38 +0800

    注解

--- mall-base/src/main/java/com/mall/mallbase/MallBaseApplication.java
+++ mall-base/src/main/java/com/mall/mallbase/MallBaseApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MallBaseApplication {
-
+    /**
+     * 主服务
+     * @param args
+     */
     public static void main(String[] args) {
         SpringApplication.run(MallBaseApplication.class, args);
     }
